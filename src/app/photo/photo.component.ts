faf93e08a7b73c2f92e6a50c3c1b310caacfa3ae
andre.lepesteur <andre.lepesteur@gmail.com>
2020-03-03 13:12:35 -0300

    Criação do primeiro component chamado photo.

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'a-photo', //letra a é de Alura, nome do curso
+  templateUrl: 'photo.component.html'
 })
-export class AppComponent {
+export class PhotoComponent {
+
   title = 'Alura';
-  description = "Le�o"
+  description = "Leão"
   url = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg"
 }
