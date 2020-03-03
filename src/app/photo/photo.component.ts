b17d55a659a212f22a0de5827a2e775899849703
andre.lepesteur <andre.lepesteur@gmail.com>
2020-03-03 13:28:37 -0300

    Inbound properties: passando dados para o componente @Input no photo.component.

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'a-photo', //letra a é de Alura, nome do curso
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 export class PhotoComponent {
 
   title = 'Alura';
-  description = "Leão"
-  url = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg"
+
+  @Input() description = ""
+  @Input() url = ""
 }
