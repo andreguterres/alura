ee7b307291cb63f3e7bfca746bf81d3079e2cbbe
andre.lepesteur <andre.lepesteur@gmail.com>
2020-03-03 16:58:19 -0300

    Ciclo de vida de um componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
@@ -6,15 +6,16 @@ import { PhotoService } from './photos/photo/photo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  photos: Object[] = [];
+  photos: any[] = [];
 
-  constructor(photoService: PhotoService) {
+  constructor(private photoService: PhotoService) { }
 
-    photoService
+  ngOnInit(): void {
+
+    this.photoService
       .listFromUser('flavio')
       .subscribe(photos => this.photos = photos);
-
-  } 
+  }
 }
