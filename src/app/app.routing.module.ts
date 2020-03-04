11ccbb4934faee926eb3254c60710e495c26d7eb
andre.lepesteur <andre.lepesteur@gmail.com>
2020-03-04 10:08:32 -0300

    Rota temporaria

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PhotoComponent } from './photos/photo/photo.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 
 const routes: Routes = [
-  { path: 'user/flavio', component: PhotoComponent },
+  { path: 'user/flavio', component: PhotoListComponent },
   { path: 'p/add', component: PhotoFormComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)]
+  imports: [RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 
 })
 export class AppRoutingModule {}
