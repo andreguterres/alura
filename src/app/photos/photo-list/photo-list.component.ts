8b80df7c4d8da669f41cfa8a5b0b913d561a218f
andre.lepesteur <andre.lepesteur@gmail.com>
2020-03-05 15:59:42 -0300

    RxJS e  Subject usando debounceTime(300)  para não fazer requisição a cada letra digitada na pesquisa.

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+//import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators'
+
 
 import { Photo } from '../photo/photo';
 
@@ -9,10 +12,14 @@ import { Photo } from '../photo/photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe(); // para não alocar dados em memoria na hora da pesquisa
+    }
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
   constructor(private activatedRoute: ActivatedRoute) { }
     //private photoService: PhotoService,
    
@@ -20,6 +27,10 @@ export class PhotoListComponent implements OnInit {
   ngOnInit(): void {
 
     this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+    
 
     //const userName = this.activatedRoute
     //  .snapshot
