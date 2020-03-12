87a94c035b6d2510d0947314a280101ea0252d96
andre.lepesteur <andre.lepesteur@gmail.com>
2020-03-12 10:14:28 -0300

    Acesso do header de resposta. Token de resposta de usuário logado.

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService
-{
+export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  authenticate(userName: string, password: string)
-  {
-    return this.http.post(API_URL + '/user/login', { userName, password })// poderia também enviar os paramentros sem precisar atribui-los,
-                                                                                         //pois o nome da propriedade é o mesmo da variavel se tratando de javaScript,
-                                                                                         //caso contrario teria que fazer atribuição se os nomes fosse diferentes.Ficaria{userName, password}
+  authenticate(userName: string, password: string) {
+    return this.http.post(API_URL + '/user/login', { userName, password }, { observe: 'response' })// poderia também enviar os paramentros sem precisar atribui-los,
+      .pipe(tap(res => {                                                                           //pois o nome da propriedade é o mesmo da variavel se tratando de javaScript,
+       const authToken = res.headers.get('x-access-token');                                       //caso contrario teria que fazer atribuição se os nomes fosse diferentes.Ficaria{userName, password}
+       console.log(`User ${userName} authenticated with token ${authToken}`)                                                                                          
+                                                                                                 
+    
+
+      }));
   }
 }
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -51,6 +51,5 @@ export class SignInComponent implements OnInit {
           alert('Usuário Invalido !');
         }
       );
-
   }
 }
