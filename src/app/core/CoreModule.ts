db4c566530f7d161a89daa2153ab35059c8a8e91
andre.lepesteur <andre.lepesteur@gmail.com>
2020-03-13 10:01:23 -0300

    Async pipe => o "|" pipe junto com o user$ com o "$" pega o valor do observable no template usando o async e falando que ele é um "user".

--- src/app/core/CoreModule.ts
+++ src/app/core/CoreModule.ts
@@ -1,9 +1,12 @@
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent],
+  imports: [CommonModule]
+
 
 })
 export class CoreModule { }
