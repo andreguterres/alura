2192267f39c8f7c3e116497f83fb1c38ca1263fe
andre.lepesteur <andre.lepesteur@gmail.com>
2020-03-13 11:14:32 -0300

    A diretiva [routerLink] => serve para que a aplicação não seja carregada po inteira ao solicitar uma rota.

--- src/app/core/CoreModule.ts
+++ src/app/core/CoreModule.ts
@@ -1,11 +1,12 @@
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule]
 
 
 })
